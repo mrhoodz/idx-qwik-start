c64eefac7778643f33ff20e490dfbe844a9f5ee5
realwebsdigital <realwebsdigital@gmail.com>
2025-02-09 13:21:27 +0000

    fix lint errors

--- src/routes/email/component/index.tsx
+++ src/routes/email/component/index.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource react */
 
-import { component$ } from '@builder.io/qwik';
 import { type RequestHandler } from '@builder.io/qwik-city';
 import { render } from '@react-email/render';
 import NotionMagicLinkEmail from '~/components/emails/notion-magic-link';
--- src/routes/react/index.tsx
+++ src/routes/react/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { qwikify$ } from "@builder.io/qwik-react";
 import NotionMagicLinkEmail from "~/components/emails/notion-magic-link";
