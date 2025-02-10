8372198bdc4110fad2c904ff0d22763b4fcfac3b
realwebsdigital@gmail.com <realwebsdigital@gmail.com>
2025-02-10 23:34:38 +0200

    updated pull

--- src/routes/email/base/index.tsx
+++ src/routes/email/base/index.tsx
@@ -1,15 +1,12 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { qwikify$ } from "@builder.io/qwik-react";
 import NotionMagicLinkEmail from "~/components/emails/notion-magic-link";
 
 export default component$(() => {
-
   return (
     <>
-
       <QNotionMagicLinkEmail loginCode="this is the login code" />
-
     </>
   );
 });
@@ -17,6 +14,5 @@ export default component$(() => {
 export const head: DocumentHead = {
   title: "React Email",
 };
-
 
 export const QNotionMagicLinkEmail = qwikify$(NotionMagicLinkEmail);
